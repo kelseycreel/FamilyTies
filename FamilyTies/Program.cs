16a7cfbaff5ea1a7db4e7777fb4f4da517056a51
Kelsey Creel <kelseycreel@gmail.com>
2020-03-21 09:41:24 -0500

    Add project files.

---
+++ FamilyTies/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FamilyTies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("big brother", new Dictionary<string, string>() { { "name", "Dillon" }, { "age", "24" } });
+            myFamily.Add("little brother", new Dictionary<string, string>() { { "name", "Luke" }, { "age", "15" } });
+            myFamily.Add("mom", new Dictionary<string, string>() { { "name", "Susan" }, { "age", "55" } });
+            myFamily.Add("dad", new Dictionary<string, string>() { { "name", "Don" }, { "age", "56" } });
+            myFamily.Add("pup", new Dictionary<string, string>() { { "name", "Stormy" }, { "age", "4" } });
+
+            //var familyMember = myFamily.Keys;
+
+            foreach (var member in myFamily)
+            {
+                var printString = $"{member.Value["name"]} is my { member.Key } and is {member.Value["age"]} years old.";
+                Console.WriteLine(printString);
+            }
+        }
+    }
+}
